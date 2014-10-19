825cb68984f751ec74b27c4c16cdb6bd36eaf5c3
Bill Kramme <bk@liquigel.net>
2014-10-19 22:55:55 +0000

    Creating new repo

---
+++ beers.py
@@ -0,0 +1,7 @@
+def listbeers():
+    return [
+    ["Schlafly Oatmeal Stout", "Oatmeal Stout", "5.7% ABV", "40 IBU"],
+    ["Shiner Bock", "Bock", "4.4% ABV", "13 IBU"],
+    ["Guinness Draught", "Irish Dry Stout", "4.2% ABV", "40 IBU"],
+    ["Narwhal", "Russian Imperial Stout", "10.2% ABV", "60 IBU"],
+]
