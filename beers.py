9f0e11e098d9c13967c798bf61bc69d2f674e8f3
Bill Kramme <billkramme@users.noreply.github.com>
2014-10-19 19:29:10 -0500

    Update beers.py
    Added origins to those missing, corrected missing brewery name for Narwhal

--- beers.py
+++ beers.py
@@ -1,8 +1,8 @@
 def listbeers():
     return [
-    ["Schlafly Oatmeal Stout", "Oatmeal Stout", "5.7% ABV", "40 IBU"],
-    ["Shiner Bock", "Bock", "4.4% ABV", "13 IBU"],
-    ["Guinness Draught", "Irish Dry Stout", "4.2% ABV", "40 IBU"],
-    ["Narwhal", "Russian Imperial Stout", "10.2% ABV", "60 IBU"],
+    ["Schlafly Oatmeal Stout", "Oatmeal Stout", "5.7% ABV", "40 IBU", "Missouri"],
+    ["Shiner Bock", "Bock", "4.4% ABV", "13 IBU", "Texas"],
+    ["Guinness Draught", "Irish Dry Stout", "4.2% ABV", "40 IBU", "Ireland"],
+    ["Sierra Nevada Narwhal", "Russian Imperial Stout", "10.2% ABV", "60 IBU", "California"],
     ["Horny Goat Exposed", "Cream Ale", "4.9% ABV", "13 IBU", "Wisconsin"]
 ]
